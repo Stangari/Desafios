f9e14302f49e4b21178698616bfbf95909fb9703
Stangari <juniorstangari@hotmail.com>
2024-08-21 22:32:20 -0300

    Equacao Finalizada

--- src/main/java/org/stangari/EquacaoDeSegundoGrau.java
+++ src/main/java/org/stangari/EquacaoDeSegundoGrau.java
@@ -3,10 +3,17 @@ package org.stangari;
 //Crie uma função que retorne o valor de uma equação de segundo grau
 
 public class EquacaoDeSegundoGrau {
-double a = 1;
-double b = 2;
-double c = 3;
-int x;
+    public static void main(String[] args) {
+        int a = 1;
+        int b = -3;
+        int c = -10;
 
-double delta = b * b - 4 * a * c;
+        double delta = (b * b) - 4 * a * c;
+
+        int raizA = (int) (-b + Math.sqrt(delta));
+        int resultA = raizA / 2 * a;
+        int raizB = (int) (-b - Math.sqrt(delta));
+        int resultB = raizB / 2 * a;
+        System.out.println(resultA +","+ resultB);
+    }
 }
